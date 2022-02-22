14884a64d3dc852210515c8886e91e9ac82cbedc
Natália <nataliasouzaguimaraes@gmail.com>
2022-02-21 23:46:56 -0300

    Commit inicial

---
+++ main.go
@@ -0,0 +1,54 @@
+package main
+
+import "fmt"
+
+type compra struct{
+	comprador string
+	qtd int
+	total float32
+}
+
+func main(){
+	var bilheteria []compra
+
+	var compraTemp compra
+	const valor = 10.50
+	var reserva string
+	var novaCompra string
+
+	fmt.Println("Bem vindo ao sistema de reserva de tickets")
+	
+	for{
+		fmt.Printf("Qual seu nome? ")
+		fmt.Scanln(&compraTemp.comprador)
+
+		fmt.Printf("Quantos tickets você quer reservar? ")
+		fmt.Scan(&compraTemp.qtd)
+		compraTemp.total = valor * float32(compraTemp.qtd)
+
+		fmt.Printf("%s, você confirma a reserva de %d tickets no valor de %.2f reais? (s/n)\n", compraTemp.comprador, compraTemp.qtd, compraTemp.total )
+		fmt.Scan(&reserva)
+
+		if (reserva == "s") {
+			bilheteria = append(bilheteria, compraTemp)
+			fmt.Println("Wow.. seus tickets foram reservados!!!")
+		}else if (reserva == "n"){
+			continue;
+		}
+
+		fmt.Println("Quer reservar mais tickets? (s/n)")
+		fmt.Scan(&novaCompra)
+
+		if (novaCompra == "n"){
+			fmt.Println("Tickets reservados: ")
+			fmt.Println( "Nome: Qtd: Total: ")
+			
+			for i := 0; i<len(bilheteria); i++ {
+				fmt.Println(bilheteria[i].comprador, " - ", bilheteria[i].qtd, " - ", bilheteria[i].total)
+			}
+			
+			break;
+		}
+
+	}
+}
